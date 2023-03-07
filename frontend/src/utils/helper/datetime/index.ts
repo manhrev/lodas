e9ef0be5f300c0d648d6ed4c98a00201f6fc101d
Manh Nguyen <manhnd@inspirelab.io>
2023-03-07 09:39:06 +0700

    fix format datetime

--- frontend/src/utils/helper/datetime/index.ts
+++ frontend/src/utils/helper/datetime/index.ts
@@ -1,27 +1,13 @@
 import { Timestamp } from "google-protobuf/google/protobuf/timestamp_pb";
+import moment from "moment";
 
 export const formatDateTime = (time: Timestamp.AsObject) => {
-  const date = new Date(time.seconds * 1000);
-  const yyyy = date.getFullYear();
-  let mm = date.getMonth() + 1; // Months start at 0!
-  let dd = date.getDate();
-  let mmstring = mm.toString();
-  let ddstring = dd.toString();
-  if (dd < 10) ddstring = "0" + dd;
-  if (mm < 10) mmstring = "0" + mm;
+  const date = moment.unix(time.seconds).format("DD/MM/YYYY HH:mm");
 
-  return dd + "/" + mm + "/" + yyyy + " " + date.toLocaleTimeString();
+  return date;
 };
 
 export const formatDate = (time: Timestamp.AsObject) => {
-  const date = new Date(time.seconds * 1000);
-  const yyyy = date.getFullYear();
-  let mm = date.getMonth() + 1; // Months start at 0!
-  let dd = date.getDate();
-  let mmstring = mm.toString();
-  let ddstring = dd.toString();
-  if (dd < 10) ddstring = "0" + dd;
-  if (mm < 10) mmstring = "0" + mm;
-
-  return dd + "/" + mm + "/" + yyyy;
+  const date = moment.unix(time.seconds).format("DD/MM/YYYY");
+  return date;
 };
